d562ebd6ac4a142190f4ebb87fba1542f0c59a80
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-02-05 19:26:25 +0100

    add loader animation

--- views/genericWidgets.py
+++ views/genericWidgets.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, ttk
 from tkinter import *
+from random import randint
 
 
 class ErrorPopup(Toplevel):
@@ -13,4 +14,23 @@ class ErrorPopup(Toplevel):
 class LoaderFrame(Frame):
     def __init__(self,parent):
         super().__init__(parent)
-        ttk.Label(self,text="Récupération en cours").pack(fill="both")
+        self.zoneLoader = 0
+        ttk.Label(self,text="Récupération en cours").pack(pady=(20,0))
+
+        self.loaderIcon = Canvas(self, width=150, height=100)
+        self.loaderIcon.pack()
+
+        self.updateLoader()
+        
+
+
+    def updateLoader(self,item=None):
+        self.loaderIcon.delete(item)
+        xPoint=(self.zoneLoader%3)*50
+        yPoint=0
+        length=50
+
+        item=self.loaderIcon.create_rectangle(xPoint, yPoint, xPoint+length, yPoint+length, fill='red', outline='')
+        self.zoneLoader+=1
+
+        self.after(1000, self.updateLoader,item)
