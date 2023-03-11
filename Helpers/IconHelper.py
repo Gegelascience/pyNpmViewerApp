aa6c3bbedb12f3ddac5635a4238ab9c60de5e39d
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-03-11 18:36:37 +0100

    simplification generateur icone

--- Helpers/IconHelper.py
+++ Helpers/IconHelper.py
@@ -1,70 +1,22 @@
 from PngHelper import PngBuilder
-
+import struct
 
 def returnIconHeaderValue():
-	# ico with 1 image
-	return [0,0,1,0,1,0]
+	return struct.pack('<hhh', 0, 1, 1)
 
-def returnIconDirectory(width, height):
-	#ico 16x16
-	directoryData = []
-	# largeur
-	directoryData.append(width)
-	# hauteur
-	directoryData.append(height)
-	
-	# 1 couleur
-	directoryData.append(1)
-	# 0 reserve
-	directoryData.append(0)
-	
-	# color plane
-	directoryData.append(0)
-	directoryData.append(0)
-	
-	# bit par pixel
-	directoryData.append(1)
-	directoryData.append(0)
-
-	# data size in bytes
-	directoryData.append(0)
-	directoryData.append(4)
-	directoryData.append(0)
-	directoryData.append(0)
-
-	# offset data
-	directoryData.append(22)
-	directoryData.append(0)
-	directoryData.append(0)
-	directoryData.append(0)
-
-
-	return directoryData
+def returnIconDirectory(width:int, height:int):
+	widthConfig = width if width < 256 else 0
+	heightConfig = height if height < 256 else 0
+	directoryBytes = struct.pack("<BBBBhhII",widthConfig, heightConfig, 1,0,0,1,width*height*4,22)
+	return directoryBytes
 
 def createIcon(actualData,width,height):
-	icoFileHeaders = []
 	
 	header = returnIconHeaderValue()
-	directory = returnIconDirectory(width,height)
-
+	icondirectory = returnIconDirectory(width,height)
 	pngElement = PngBuilder(actualData,width,height)
 
-	icoFileHeaders.extend(header)
-	icoFileHeaders.extend(directory)
-
-	icoBytes = []
-	for byte in icoFileHeaders:
-		#print("byte",byte )
-		icoBytes.append(byte.to_bytes(1, byteorder='little'))
-
-	for el in pngElement.byteContentList:
-		icoBytes.append(el)
-
-	return icoBytes
-
-	
-
-
+	return b"".join([header,icondirectory,pngElement.binaryContent])
 
 
 if __name__ == "__main__":
@@ -74,14 +26,12 @@ if __name__ == "__main__":
 		col = 0
 		rowData = []
 		while col < 32:
-			rowData.append([255,255,255,255])
+			rowData.append([255,127,0,255])
 			col+=1
 		actualData.append(rowData)	
 		row+=1
 
-	iconFileContent =createIcon(actualData, 32,32)
+	iconFilebinary =createIcon(actualData, 32,32)
 
 	with open("test.ico","wb") as icoFile:
-
-		for el in iconFileContent:
-			icoFile.write(el)
+		icoFile.write(iconFilebinary)
