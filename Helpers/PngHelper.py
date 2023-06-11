ba80ba7c9a8552cb44f8ff1ee0c689809f86ea6d
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-06-11 17:47:00 +0200

    helper cleaner

--- Helpers/PngHelper.py
+++ Helpers/PngHelper.py
@@ -1,5 +1,30 @@
 import zlib
 import struct
+from enum import Enum, unique
+
+@unique
+class PngChunkName(Enum):
+	IHDR="IHDR"
+	IDAT="IDAT"
+	IEND="IEND"
+	PLTE="PLTE"
+
+class PngChunkBuilder:
+
+	def __init__(self,chunkName:PngChunkName,data:bytes):
+		self.__chunkType = chunkName.value.encode("ascii")
+		if chunkName != PngChunkName.IEND:
+			self.__chunkData = data
+			self.__chunkDataLen = struct.pack('>I', len(data))
+			if chunkName ==PngChunkName.PLTE and len(data)%3 !=0:
+				raise Exception("invalid palette data")
+		else:
+			self.__chunkData= "".encode()
+			self.__chunkDataLen =struct.pack('>I', 0)
+		self.__chunkCRC = struct.pack('>I', zlib.crc32(self.__chunkData, zlib.crc32(struct.pack('>4s', self.__chunkType))))
+
+	def getBytesContent(self):
+		return b"".join([self.__chunkDataLen,self.__chunkType, self.__chunkData, self.__chunkCRC])
 
 class PngBuilder:
 	binaryContent:bytes
@@ -9,16 +34,10 @@ class PngBuilder:
 		pngBytesNearlyOK = []
 
 		# magic number
-		pngBytesNearlyOK.extend(struct.pack('>BBBBBBBB', 137, 80, 78, 71, 13, 10, 26,10))
+		pngBytesNearlyOK.append(struct.pack('>BBBBBBBB', 137, 80, 78, 71, 13, 10, 26,10))
 		
 		# IDHR
-		IHDR = ['', '', '', ''] # Les 4 éléments d'un bloc
-		IHDR[1] = u'IHDR'.encode('ascii')
-		IHDR[2] = struct.pack('>IIBBBBB', width, height, 8, 6, 0, 0, 0)
-		IHDR[0] = struct.pack('>I', len(IHDR[2]))
-		IHDR[3] = struct.pack('>I', zlib.crc32(IHDR[2], zlib.crc32(struct.pack('>4s', u'IHDR'.encode('ascii')))))
-
-		pngBytesNearlyOK.extend(IHDR)
+		pngBytesNearlyOK.append(PngChunkBuilder(PngChunkName.IHDR,struct.pack('>IIBBBBB', width, height, 8, 6, 0, 0, 0)).getBytesContent())
 
 		# IDAT
 		image = []
@@ -32,37 +51,12 @@ class PngBuilder:
 
 		image_compressee = zlib.compress(bytearray(image))
 
-		
-		IDAT = ['', '', '', '']
-		IDAT[1] = u'IDAT'.encode('ascii')
-		IDAT[2] = image_compressee
-		IDAT[0] = struct.pack('>I', len(IDAT[2]))
-		IDAT[3] = struct.pack('>I', zlib.crc32(IDAT[2], zlib.crc32(struct.pack('>4s', u'IDAT'.encode('ascii')))))
-
-		pngBytesNearlyOK.extend(IDAT)
+		pngBytesNearlyOK.append(PngChunkBuilder(PngChunkName.IDAT,image_compressee).getBytesContent())
 
 		# IEND
-		IEND = ['', '', '', '']
-		IEND[1] = u'IEND'.encode('ascii')
-		IEND[0] = struct.pack('>I', len(IEND[2]))
-		IEND[3] = struct.pack('>I', zlib.crc32(IEND[2].encode(), zlib.crc32(struct.pack('>4s', u'IEND'.encode('ascii')))))
-		
-		pngBytesNearlyOK.extend(IEND)
+		pngBytesNearlyOK.append(PngChunkBuilder(PngChunkName.IEND,b"").getBytesContent())
 
-
-		# formatage du contenu en bytes
-
-		byteContentList = []
-		for el in pngBytesNearlyOK:
-			if isinstance(el,int):
-				byteContentList.append(el.to_bytes(1, byteorder='little'))
-			elif isinstance(el,str):
-				if len(el) > 0:
-					byteContentList.append(bytearray(el,encoding="utf-8"))
-			else:
-				byteContentList.append(el)
-
-		self.binaryContent = b"".join(byteContentList)
+		self.binaryContent = b"".join(pngBytesNearlyOK)
 
 
 	def writeFile(self,filePath:str):
