b54ff0baf4676da51d6c20f6dca2ca861ea65588
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-06-25 13:53:51 +0200

    écriture du resultat dans un fichier

--- testRunner.py
+++ testRunner.py
@@ -1,5 +1,11 @@
 from Helpers.tests import *
 import unittest
+from io import StringIO
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    reportTest = StringIO()
+    runner = unittest.TextTestRunner(reportTest,descriptions=True,verbosity=2)
+    unittest.main(verbosity=2,buffer=False,exit=False, testRunner=runner)
+
+    with open("report.txt","w",encoding="utf-8") as file:
+        file.write(reportTest.getvalue())
